c8af4d8ac25988e60cdf23cc7622a7c1def51e4c
Lucas <lucasbombaron@gmail.com>
2022-07-26 10:45:06 +0200

    MAJ

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,34 +1,73 @@
-//======== RÉCUPÉRATION DES DONNÉES PHOTOGRAPHES JSON ========
+//============ RECUPERATION DES DONNEES JSON "PHOTOGRAPHERS" ============
 async function getPhotographers() {
-  
-    await fetch('./data/photographers.json', {mode : 'cors'})
-      .then (res => res.json())
-      .then((data) => (photographers = data.photographers));
-    return {
-      photographers: [...photographers]
-    };
+        
+	await fetch("./data/photographers.json")
+		.then((res) => res.json())
+		// eslint-disable-next-line no-undef
+		.then((data) => (photographers = data.photographers));
+
+	return {
+		// eslint-disable-next-line no-undef
+		photographers: [...photographers]            
+	};
 }
 
-//======== RÉCUPÉRATION DES DONNÉES MEDIA JSON ========
-async function getMedia() {
-
-    await fetch('./data/photographers.json')
-        .then((res) => res.json())
-        .then((data) => (media = data.media));
-
-    return {
-        media : [...media]
-    };
+//============== RECUPERATION DES DONNEES JSON "MEDIA" ===================
+async function getMedia() {       
+	await fetch("./data/photographers.json")
+		.then((res) => res.json())
+		// eslint-disable-next-line no-undef
+		.then((data) => (media = data.media));
+    
+	return {
+		// eslint-disable-next-line no-undef
+		media : [...media]
+	};            
 }
 
-//======== AFFICHAGE DES PHOTOS DE LA PAGE PHOTOGRAPHERS ========
+//=================== AFFICHAGE DE LA GALERIE PHOTO DE LA PAGE PHOTOGRAPHER =================
 async function displayDataGalery(media) {
-    const photographersGalerySection = document.querySelector('.photographerGalery');
-    media.foreach(itemMedia => {
-        if(urlIdPhotographer === itemMedia.photographerID) {
-            const photographersGaleryModel = photographerMediaFactory(itemMedia);
-            const userGaleryCardDOM = photographerGaleryModel.gatGaleryCardDom();
-            photographersGalerySection.appendChild(userGaleryCardDOM);
-        }
-    });
+	const photographersGalerySection = document.querySelector(".photographerGalery");
+	media.forEach(itemMedia => {
+		// eslint-disable-next-line no-undef
+		if(urlIdPhotographer === itemMedia.photographerId) {
+			// eslint-disable-next-line no-undef
+			const photographerGaleryModel = photographerMediaFactory(itemMedia);
+			const userGaleryCardDOM = photographerGaleryModel.getGaleryCardDom();
+			photographersGalerySection.appendChild(userGaleryCardDOM);       
+		}
+	});
 }
+
+//==================== AFFICHAGE DES PROFILS DE LA PAGE PHOTOGRAPHER ====================
+async function displayDataPhotographer(photographers) {
+	photographers.forEach(photographer => {
+		// eslint-disable-next-line no-undef
+		if(urlIdPhotographer === photographer.id) {
+			// eslint-disable-next-line no-undef
+			photographerHeaderPage(photographer);
+		}
+	});
+}
+
+async function init() {
+	// Récupère les datas des photographes
+	const { photographers } = await getPhotographers();
+	const { media } = await getMedia();
+	displayDataGalery(media);
+	displayDataPhotographer(photographers);
+	// eslint-disable-next-line no-undef
+	getTotalLike(media, photographers);
+	// eslint-disable-next-line no-undef
+	validate();
+	// eslint-disable-next-line no-undef
+	Lightbox.init();
+	// eslint-disable-next-line no-undef
+	// sortPhoto(media);
+	// eslint-disable-next-line no-undef
+	trapFocus(modal);  
+	// eslint-disable-next-line no-undef
+	sort();
+}
+init();
+
